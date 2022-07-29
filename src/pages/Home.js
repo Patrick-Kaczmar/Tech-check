93982c919cc23f5928323076d44e1235c9c5da2a
Patrick Kaczmar <pk1blue@yahoo.com>
2022-07-29 04:43:22 -0400

    added navbar initial design

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ResponsiveAppBar from '../components/Navbar';
+
+export default function Home() {
+    return (
+        <>
+            <ResponsiveAppBar />
+        </>
+    )
+}
