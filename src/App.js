fe28d716db333a7b765e56e6277df3eac29355f2
Patrick Kaczmar <pk1blue@yahoo.com>
2022-07-29 13:36:58 -0400

    set up react router for pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import ResponsiveAppBar from './components/Navbar';
 import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Store from './pages/Store';
 
 function App() {
   return (
     <>
-      <Home />
+      <ResponsiveAppBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/store' element={<Store />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
     </>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import ResponsiveAppBar from '../components/Navbar';
 
 export default function Home() {
     return (
         <>
-            <ResponsiveAppBar />
+            <div>this is the home page</div>
         </>
     )
 }
