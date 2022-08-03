5077531e3d1268d8c04f9a4824b86ed6cc80e173
Patrick Kaczmar <pk1blue@yahoo.com>
2022-08-03 18:16:47 -0400

    implemented products and cart from commerce.js

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -3,25 +3,24 @@ import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } fro
 import { AddShoppingCart } from '@mui/icons-material'
 import styles from  '../../../assets/css/product.module.css'
 
-export default function Product({props}) {
+export default function Product({props, onAddToCart}) {
+
   return (
     <Card className={styles.root}>
-        <CardMedia className={styles.media} image={props.image} title={props.name} />
+        <CardMedia className={styles.media} image={props.image.url} title={props.name} />
         <CardContent>
             <div className={styles.cardContent}>
                 <Typography variant='h5' gutterBottom>
                     {props.name}
                 </Typography>
                 <Typography variant='h5'>
-                    {props.price}
+                    {props.price.formatted_with_symbol}
                 </Typography>
             </div>
-            <Typography variant='body2'>
-                {props.description}
-            </Typography>
+            <Typography dangerouslySetInnerHTML={{__html: props.description}} variant='body2' />
         </CardContent>
         <CardActions disableSpacing className={styles.cardActions}>
-            <IconButton aria-label='Add to Cart'>
+            <IconButton aria-label='Add to Cart' onClick={() => onAddToCart(props.id, 1)}>
                 <AddShoppingCart />
             </IconButton>
         </CardActions>
--- src/components/Products/products.jsx
+++ src/components/Products/products.jsx
@@ -3,31 +3,14 @@ import { Grid } from '@mui/material'
 import Product from './Product/Product'
 import styles from '../../assets/css/products.module.css'
 
-const products = [
-    {
-        id: 1,
-        name: 'shoes',
-        description: 'running shoes',
-        price: '$5',
-        image: 'https://www.popsci.com/uploads/2021/01/14/54BGRKCC7BEAPGZ3XAK5RHJF6M.jpg?auto=webp'
-    },
-    {
-        id: 2,
-        name: 'Macbook',
-        description: 'Apple macbook',
-        price: '$10',
-        image: 'https://cdn.pocket-lint.com/r/s/970x/assets/images/152137-laptops-review-apple-macbook-pro-2020-review-image1-pbzm4ejvvs.jpg'
-    }
-]
-
-export default function Products() {
+export default function Products({ products, onAddToCart }) {
     return (
         <main className={styles.content}>
             <div className={styles.toolbar}/>
             <Grid container justifyContent={'center'} spacing={4}>
                 {products.map((item) => (
                     <Grid item key={item.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product props={item} />
+                        <Product props={item} onAddToCart={onAddToCart}/>
                     </Grid>
                 ))}
             </Grid>
